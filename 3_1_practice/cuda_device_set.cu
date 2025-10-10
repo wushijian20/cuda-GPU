627978b3e797fffa7d6257c4f06517fc00644ddd
wushijian20 <wushijian20@gmail.com>
2025-10-09 22:02:02 -0400

    check GPU No. and setup GPU

---
+++ 3_1_practice/cuda_device_set.cu
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int iDeviceCount = 0;
+    cudaError_t error = cudaGetDeviceCount(&iDeviceCount);
+
+    if(error != cudaSuccess || iDeviceCount == 0)
+    {
+        printf("No CUDA GPU found!\n");
+        exit(-1);
+    }
+    else
+    {
+        printf("The number of Cuda GPU(s): %d\n", iDeviceCount);
+
+    }
+
+    int iDev = 0;
+    error = cudaSetDevice(iDev);
+    if(error != cudaSuccess)
+    {
+        printf("fail to set GPU %d as the current device\n", iDev);
+        exit(-1);
+    }
+    else
+    {
+        printf("set GPU %d for the current device\n", iDev);
+    }
+
+    return 0;
+}
