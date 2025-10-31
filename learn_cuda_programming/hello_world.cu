c50dadae6635f733682349cdf9ef7b9b3edbfaee
wushijian20 <wushijian20@gmail.com>
2025-10-30 22:11:20 -0400

    a new book

---
+++ learn_cuda_programming/hello_world.cu
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+__global__ void print_from_gpu(void) {
+    printf("Hello World! from thread [%d, %d] \
+        from device\n", threadIdx.x, blockIdx.x);
+}
+
+int main(void) {
+    printf("Hello World from host!\n");
+    print_from_gpu<<<1,1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
